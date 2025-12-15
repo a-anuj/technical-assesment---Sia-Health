39aec301e5a38b898c7ce3c3781ac929dcdd0c31
a-anuj <anujamirthalingam5@gmail.com>
2025-12-15 10:36:05 +0530

    chore : normalize the meal plan

---
+++ src/utils/normalizeMealPlan.js
@@ -0,0 +1,14 @@
+export function normalizeMealPlan(rawPlan) {
+  const days = ["Day1", "Day2", "Day3", "Day4", "Day5", "Day6", "Day7"];
+  const normalized = {};
+
+  days.forEach(day => {
+    normalized[day] = {
+      breakfast: rawPlan.breakfast_9_10_am?.[day],
+      lunch: rawPlan.lunch_1_2_pm?.[day],
+      dinner: rawPlan.dinner_7_8_pm?.[day]
+    };
+  });
+
+  return normalized;
+}
