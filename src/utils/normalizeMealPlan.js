b4815160ea27f68e72cff62efbde82b71b7dd16a
a-anuj <anujamirthalingam5@gmail.com>
2025-12-15 19:28:43 +0530

    chore : include the snacks portion also

--- src/utils/normalizeMealPlan.js
+++ src/utils/normalizeMealPlan.js
@@ -1,11 +1,13 @@
 export function normalizeMealPlan(rawPlan) {
-  const days = ["Day1", "Day2", "Day3", "Day4", "Day5", "Day6", "Day7"];
+  const days = ["Day1","Day2","Day3","Day4","Day5","Day6","Day7"];
   const normalized = {};
 
   days.forEach(day => {
     normalized[day] = {
       breakfast: rawPlan.breakfast_9_10_am?.[day],
+      midMorningSnack: rawPlan.midMorning_11_am?.[day],
       lunch: rawPlan.lunch_1_2_pm?.[day],
+      eveningSnack: rawPlan.eveningSnack_5_pm?.[day],
       dinner: rawPlan.dinner_7_8_pm?.[day]
     };
   });
